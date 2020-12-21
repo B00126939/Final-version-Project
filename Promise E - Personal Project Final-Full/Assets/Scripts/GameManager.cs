5727e6f9406719b813e5bac5a9d27a964a24bb18
B00126939 <71830313+B00126939@users.noreply.github.com>
2020-12-21 10:52:14 +0000

    webgl and project added + everything

---
+++ Promise E - Personal Project Final-Full/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+   public TextMeshProUGUI gameOverText;
+   public Button restartButton;
+
+   void Start()
+   {
+     
+   }
+
+   public void EndGame()
+   {
+       Debug.Log("GAME OVER");
+       gameOverText.gameObject.SetActive(true);
+       restartButton.gameObject.SetActive(true);
+   }
+
+   public void NoEndGame()
+   {      
+       gameOverText.gameObject.SetActive(false);
+       restartButton.gameObject.SetActive(false);
+   }
+
+   public void RestartGame()
+   {
+       
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+   }
+}
